5755efb272a6f42685461c56ed3fa91ea433b246
Ilaria <ilaria.bertoldi.95@gmail.com>
2023-05-06 12:13:55 +0200

    changed api baseURL

--- client/app/src/api/userAPI.js
+++ client/app/src/api/userAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({ baseURL: 'https://green-lifestyle-s2i-final-project-1hmt.vercel.app/' })
 
 export const getUser = (userId) => API.get(`/home`, userId)
 export const updateUserData = (userId, property, value) => API.patch(`/home`, { userId, property, value })
