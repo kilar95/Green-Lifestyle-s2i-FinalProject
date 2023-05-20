6e80a6fb0299b7f955da03492a619f653954dc91
Ilaria <ilaria.bertoldi.95@gmail.com>
2023-05-20 14:46:00 +0200

    mod server res

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000
 // connect to the database
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => app.listen(port, () => {
-        console.log(`Listening to port ${port}`)
+        console.log(`Listening to port ${port}`);
+        res.send(`connected to database and listening on port ${port}`);
     }))
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
