0e5a95120854ac8bdf216017162d1ec725d5020c
Ilaria <ilaria.bertoldi.95@gmail.com>
2023-06-24 11:53:15 +0200

    prova

--- server/app.js
+++ server/app.js
@@ -24,6 +24,28 @@ app.all('*', (req, res, next) => {
     next(error)
 })
 
+const mongoose = require('mongoose')
+const app = require('./app.js')
+require('dotenv').config()
+
+const port = process.env.PORT || 3000
+
+console.log("in server.js");
+
+// connect to the database
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        // Add a test route to check the database connection
+        app.get('/test-connection', (req, res) => {
+            res.send('Connected to MongoDB');
+        });
+
+        app.listen(port, () => {
+            console.log(`Listening to port ${port}`);
+        });
+    })
+    .catch((err) => console.log(err));
+
 // errors handling 
 app.use((err, req, res, next) => {
     res.status(err.status || 500)
